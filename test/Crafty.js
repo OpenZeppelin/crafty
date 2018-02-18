ec0edd3bd4ed52b22c4b01ba7cbf6b70ecfd2548
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-02-18 12:32:59 -0300

    Added rule tests.

--- test/Crafty.js
+++ test/Crafty.js
@@ -1,49 +1,63 @@
-const expectPromiseThrow = require("./helpers/expectPromiseThrow");
 const _ = require("underscore");
+const fs = require("fs");
+const expectPromiseThrow = require("./helpers/expectPromiseThrow");
 
 const Crafty = artifacts.require("Crafty");
 
 contract("Crafty", accounts => {
   let crafty = null;
+  const rules = JSON.parse(fs.readFileSync("./app/rules.json", "utf8"));
   const player = accounts[0];
 
-  beforeEach(async() => {
-    crafty = await Crafty.new();
+  describe("rules", () => {
+    it("has resources", () => {
+      assert.isAtLeast(rules.resources.length, 1);
+    });
+
+    it("resources are strings", () => {
+      rules.resources.forEach(res => {
+        assert.typeOf(res, 'string');
+      });
+    });
   });
 
-  it("player starts with no resources", async() => {
-    const resources = await crafty.resourcesOf(player, "wood");
-    assert(resources.eq(0));
-  });
+  describe("contract", () => {
+    beforeEach(async() => {
+      crafty = await Crafty.new();
+    });
 
-  it("resources increase by acquiring them", async() => {
-    await crafty.getResource("wood");
+    it("player starts with no resources", async() => {
+      const resources = await crafty.resourcesOf(player, rules.resources[0]);
+      assert(resources.eq(0));
+    });
 
-    const balance = await crafty.resourcesOf(player, "wood");
-    assert(balance.eq(1));
-  });
+    it("resources increase by acquiring them", async() => {
+      await crafty.getResource(rules.resources[0]);
 
-  it("resources can be acquired multiple times", async() => {
-    Promise.all(_.range(5).map(() => {
-      crafty.getResource("wood");
-    }));
+      const balance = await crafty.resourcesOf(player, rules.resources[0]);
+      assert(balance.eq(1));
+    });
 
-    const balance = await crafty.resourcesOf(player, "wood");
-    assert(balance.eq(5));
-  });
+    it("resources can be acquired multiple times", async() => {
+      Promise.all(_.range(5).map(() => {
+        crafty.getResource(rules.resources[0]);
+      }));
 
-  it("multiple resource types can be acquired", async() => {
-    await crafty.getResource("wood");
-    await crafty.getResource("iron");
+      const balance = await crafty.resourcesOf(player, rules.resources[0]);
+      assert(balance.eq(5));
+    });
 
-    const wood_balance = await crafty.resourcesOf(player, "wood");
-    const iron_balance = await crafty.resourcesOf(player, "iron");
+    it("multiple resource types can be acquired", async() => {
+      await Promise.all(rules.resources.map(res => crafty.getResource(res)));
 
-    assert(wood_balance.eq(1));
-    assert(iron_balance.eq(1));
-  });
+      const balances = await Promise.all(rules.resources.map(res => crafty.resourcesOf(player, res)));
+      balances.forEach(balance => {
+        assert(balance.eq(1));
+      })
+    });
 
-  it("invalid resources cannot be aquired", async () => {
-    await expectPromiseThrow(crafty.getResource(""));
+    it("invalid resources cannot be aquired", async () => {
+      await expectPromiseThrow(crafty.getResource(""));
+    });
   });
 });
--- truffle.js
+++ truffle.js
@@ -5,5 +5,10 @@ module.exports = {
       port: 8545,
       network_id: "*"
     }
+  },
+
+  mocha: {
+    timeout: 1000,
+    slow: 300
   }
 };
