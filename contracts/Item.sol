c9d99c80521220b39ec137ad6523fd05bf3fea8b
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-02-27 23:02:14 -0300

    Now calling SafeMath with the using-for syntax.

--- contracts/Item.sol
+++ contracts/Item.sol
@@ -4,15 +4,17 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract Item is Ownable {
+  using SafeMath for uint256;
+
   mapping (address => uint256) private balances;
 
   function add(address player, uint256 amount) public onlyOwner {
-    balances[player] = SafeMath.add(balances[player], amount);
+    balances[player] = balances[player].add(amount);
   }
 
   function subtract(address player, uint256 amount) public onlyOwner {
     require(balances[player] >= amount);
-    balances[player] = SafeMath.sub(balances[player], amount);
+    balances[player] = balances[player].sub(amount);
   }
 
   function amount(address player) public view returns (uint256) {
