efa5ffb1190752c8135140357c66597ecd5db577
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-02-27 22:24:43 -0300

    Removed unnecessary testing script.

--- truffle.js
+++ truffle.js
@@ -1,12 +1,4 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*"
-    }
-  },
-
   mocha: {
     timeout: 6000,
     slow: 1000
