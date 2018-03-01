5385f6236bf45d29ef526fa3655cd0df4e4739ad
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-02-28 21:11:41 -0300

    Added recipe structs, keeping interface for now.

--- contracts/Crafty.sol
+++ contracts/Crafty.sol
@@ -3,121 +3,155 @@ pragma solidity ^0.4.17;
 import './Item.sol';
 
 contract Crafty {
-  Item private wood;
-  Item private stone;
-  Item private bronze;
-  Item private iron;
-  Item private stoneSword;
-  Item private bronzeSword;
-  Item private ironSword;
-  Item private triSword;
+  struct RecipeIngredient {
+    Item ingredient;
+    uint256 amountNeeded;
+  }
+
+  struct Craftable {
+    Item item;
+    RecipeIngredient[] recipe;
+  }
+
+  enum ItemId {
+    Wood,
+    Stone,
+    Bronze,
+    Iron,
+    StoneSword,
+    BronzeSword,
+    IronSword,
+    TriSword
+  }
+
+  mapping (bytes32 => Craftable) private craftables;
 
   function Crafty() public {
-    wood = new Item();
-    stone = new Item();
-    bronze = new Item();
-    iron = new Item();
-    stoneSword = new Item();
-    bronzeSword = new Item();
-    ironSword = new Item();
-    triSword = new Item();
+    // Basic items - empty recipes
+
+    createItem(ItemId.Wood);
+    createItem(ItemId.Stone);
+    createItem(ItemId.Bronze);
+    createItem(ItemId.Iron);
+
+    // Advanced items
+
+    createItem(ItemId.StoneSword);
+    addIngredient(ItemId.StoneSword, ItemId.Wood, 1);
+    addIngredient(ItemId.StoneSword, ItemId.Stone, 3);
+
+    createItem(ItemId.BronzeSword);
+    addIngredient(ItemId.BronzeSword, ItemId.Wood, 1);
+    addIngredient(ItemId.BronzeSword, ItemId.Bronze, 3);
+
+    createItem(ItemId.IronSword);
+    addIngredient(ItemId.IronSword, ItemId.Wood, 1);
+    addIngredient(ItemId.IronSword, ItemId.Iron, 3);
+
+    createItem(ItemId.TriSword);
+    addIngredient(ItemId.TriSword, ItemId.StoneSword, 1);
+    addIngredient(ItemId.TriSword, ItemId.BronzeSword, 1);
+    addIngredient(ItemId.TriSword, ItemId.IronSword, 1);
+  }
+
+  function createItem(ItemId id) private {
+    craftables[keccak256(id)].item = new Item();
+  }
+
+  function getItem(ItemId id) private view returns (Item) {
+    return craftables[keccak256(id)].item;
+  }
+
+  function addIngredient(ItemId result, ItemId ingredientId, uint256 amount) private {
+    craftables[keccak256(result)].recipe.push(RecipeIngredient({
+      ingredient: getItem(ingredientId),
+      amountNeeded: amount
+    }));
   }
 
   // Item acquisition
 
   function acquireWood() public {
-    wood.add(msg.sender, 1);
+    acquireItem(ItemId.Wood);
   }
 
   function acquireStone() public {
-    stone.add(msg.sender, 1);
+    acquireItem(ItemId.Stone);
   }
 
   function acquireBronze() public {
-    bronze.add(msg.sender, 1);
+    acquireItem(ItemId.Bronze);
   }
 
   function acquireIron() public {
-    iron.add(msg.sender, 1);
+    acquireItem(ItemId.Iron);
+  }
+
+  function acquireItem(ItemId id) public {
+    address player = msg.sender;
+    RecipeIngredient[] memory ingredients = craftables[keccak256(id)].recipe;
+
+    uint i;
+    // Make sure the player has enough items to acquire the target item
+    for (i = 0; i < ingredients.length; ++i) {
+      require(ingredients[i].ingredient.amount(player) >= ingredients[i].amountNeeded);
+    }
+
+    // Substract the materials
+    for (i = 0; i < ingredients.length; ++i) {
+      ingredients[i].ingredient.subtract(player, ingredients[i].amountNeeded);
+    }
+
+    getItem(id).add(player, 1);
   }
 
   function acquireStoneSword() public {
-    require(amountWood() >= 1);
-    require(amountStone() >= 3);
-
-    address player = msg.sender;
-
-    wood.subtract(player, 1);
-    stone.subtract(player, 3);
-    stoneSword.add(player, 1);
+    acquireItem(ItemId.StoneSword);
   }
 
   function acquireBronzeSword() public {
-    require(amountWood() >= 1);
-    require(amountBronze() >= 3);
-
-    address player = msg.sender;
-
-    wood.subtract(player, 1);
-    bronze.subtract(player, 3);
-    bronzeSword.add(player, 1);
+    acquireItem(ItemId.BronzeSword);
   }
 
   function acquireIronSword() public {
-    require(amountWood() >= 1);
-    require(amountIron() >= 3);
-
-    address player = msg.sender;
-
-    wood.subtract(player, 1);
-    iron.subtract(player, 3);
-    ironSword.add(player, 1);
+    acquireItem(ItemId.IronSword);
   }
 
   function acquireTriSword() public {
-    require(amountStoneSword() >= 1);
-    require(amountBronzeSword() >= 1);
-    require(amountIronSword() >= 1);
-
-    address player = msg.sender;
-
-    stoneSword.subtract(player, 1);
-    bronzeSword.subtract(player, 1);
-    ironSword.subtract(player, 1);
-    triSword.add(player, 1);
+    acquireItem(ItemId.TriSword);
   }
 
   // Item amount query
 
   function amountWood() public view returns (uint256) {
-    return wood.amount(msg.sender);
+    return getItem(ItemId.Wood).amount(msg.sender);
   }
 
   function amountStone() public view returns (uint256) {
-    return stone.amount(msg.sender);
+    return getItem(ItemId.Stone).amount(msg.sender);
   }
 
   function amountBronze() public view returns (uint256) {
-    return bronze.amount(msg.sender);
+    return getItem(ItemId.Bronze).amount(msg.sender);
   }
 
   function amountIron() public view returns (uint256) {
-    return iron.amount(msg.sender);
+    return getItem(ItemId.Iron).amount(msg.sender);
   }
 
   function amountStoneSword() public view returns (uint256) {
-    return stoneSword.amount(msg.sender);
+    return getItem(ItemId.StoneSword).amount(msg.sender);
   }
 
   function amountBronzeSword() public view returns (uint256) {
-    return bronzeSword.amount(msg.sender);
+    return getItem(ItemId.BronzeSword).amount(msg.sender);
   }
 
   function amountIronSword() public view returns (uint256) {
-    return ironSword.amount(msg.sender);
+    return getItem(ItemId.IronSword).amount(msg.sender);
   }
 
   function amountTriSword() public view returns (uint256) {
-    return triSword.amount(msg.sender);
+    return getItem(ItemId.TriSword).amount(msg.sender);
   }
 }
