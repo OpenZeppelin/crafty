97f2f14160bd690f597a3ad6f049763eead87148
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-03-03 00:07:25 -0300

    Added Item event tests.

--- test/Item.js
+++ test/Item.js
@@ -6,6 +6,7 @@ contract('Item', accounts => {
   let item = null;
   const owner = accounts[0]; // This is the account that creates the contract
   const players = accounts.slice(1, accounts.length);
+  const zeroAddress = '0x0000000000000000000000000000000000000000';
 
   beforeEach(async () => {
     item = await Item.new({from: owner});
@@ -16,7 +17,7 @@ contract('Item', accounts => {
     assert(balances.every(balance => balance.eq(0)));
   });
 
-  it('the owner can increase the balance of players selectively', async () => {
+  it('the owner can mint new items for players selectively', async () => {
     await item.mint(players[0], 10, {from: owner});
     await item.mint(players[1], 20, {from: owner});
 
@@ -27,7 +28,22 @@ contract('Item', accounts => {
     assert(balances.slice(2, balances.length).every(balance => balance.eq(0)));
   });
 
-  it('balance can be increased multiple times', async () => {
+  it('events are emitted when items are minted', async () => {
+    const {logs} = await item.mint(players[0], 10, {from: owner});
+
+    const mintEvents = logs.filter(log => log.event === 'Mint');
+    assert.equal(mintEvents.length, 1);
+    assert.equal(mintEvents[0].args.player, players[0]);
+    assert(mintEvents[0].args.amount.eq(10));
+
+    const transferEvents = logs.filter(log => log.event === 'Transfer');
+    assert.equal(transferEvents.length, 1);
+    assert.equal(transferEvents[0].args.from, zeroAddress);
+    assert.equal(transferEvents[0].args.to, players[0]);
+    assert(transferEvents[0].args.value.eq(10));
+  });
+
+  it('items can be minted multiple times', async () => {
     await item.mint(players[0], 10, {from: owner});
     await item.mint(players[0], 20, {from: owner});
 
@@ -35,7 +51,7 @@ contract('Item', accounts => {
     assert(balance.eq(30));
   });
 
-  it('non-owners cannot increase balances', async () => {
+  it('non-owners cannot mint items', async () => {
     await expectPromiseThrow(item.mint(players[0], 10, {from: players[0]}));
     await expectPromiseThrow(item.mint(players[1], 10, {from: players[0]}));
 
@@ -43,7 +59,7 @@ contract('Item', accounts => {
     assert(balances.every(balance => balance.eq(0)));
   });
 
-  it('the owner can decrease the balance of players selectively', async () => {
+  it('the owner can burn items of players selectively', async () => {
     await item.mint(players[0], 10, {from: owner});
     await item.mint(players[1], 20, {from: owner});
 
@@ -57,7 +73,17 @@ contract('Item', accounts => {
     assert(balances.slice(2, balances.length).every(balance => balance.eq(0)));
   });
 
-  it('balance can be decreased multiple times', async () => {
+  it('events are emitted when items are burned', async () => {
+    await item.mint(players[0], 10, {from: owner});
+    const {logs} = await item.burn(players[0], 5, {from: owner});
+
+    const burnEvents = logs.filter(log => log.event === 'Burn');
+    assert.equal(burnEvents.length, 1);
+    assert.equal(burnEvents[0].args.player, players[0]);
+    assert(burnEvents[0].args.amount.eq(5));
+  });
+
+  it('items can be burned multiple times', async () => {
     await item.mint(players[0], 20, {from: owner});
 
     await item.burn(players[0], 6, {from: owner});
@@ -67,11 +93,11 @@ contract('Item', accounts => {
     assert(balance.eq(10));
   });
 
-  it('balance cannot be negative', async () => {
+  it('item amounts cannot be negative', async () => {
     await expectPromiseThrow(item.burn(players[0], 1, {from: owner}));
   });
 
-  it('non-owners cannot decrease balances', async () => {
+  it('non-owners cannot burn items', async () => {
     await item.mint(players[0], 10, {from: owner});
     await item.mint(players[1], 20, {from: owner});
 
