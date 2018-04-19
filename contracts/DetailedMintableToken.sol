4cdaddcaeb9330cac11bad40eaaa6d5371d3e6c6
Leo Arias <yo@elopio.net>
2018-04-19 22:08:34 +0000

    tests: add and follow solium rules

--- contracts/DetailedMintableToken.sol
+++ contracts/DetailedMintableToken.sol
@@ -1,8 +1,15 @@
 pragma solidity ^0.4.21;
 
-import 'openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol';
-import 'openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 contract DetailedMintableToken is DetailedERC20, MintableToken {
-  function DetailedMintableToken(string _name, string _symbol, uint8 _decimals) DetailedERC20(_name, _symbol, _decimals) public {}
+  function DetailedMintableToken(
+    string _name,
+    string _symbol,
+    uint8 _decimals
+  )
+    DetailedERC20(_name, _symbol, _decimals)
+    public
+  {}
 }
