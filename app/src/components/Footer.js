8b8f2e047365731b479bd438c02c1fe7c331a09f
Delfina <delfinapolito@gmail.com>
2018-05-22 09:36:38 -0300

    footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -5,11 +5,16 @@ import Emoji from './Emoji'
 import './Footer.css'
 
 const Footer = () => (
-  <footer className='grid-x grid-padding-x align-bottom'>
+  <footer className='grid-x grid-padding-x space-between flex-center'>
+  	<div>
+  		<a target="_blank" href="https://github.com/zeppelinos/crafty">
+  			<img alt='Github' className='github' src='./images/github.svg'/>
+  		</a>
+  	</div>
     <div className='cell auto'>
       <p>
-        <Emoji e='🎈' /> An <a href='https://XLNT.co'>XLNT</a> project
-        by <a href='https://zeppelin.solutions'>Zeppelin</a>
+        <Emoji e='🎈' /> An <a target="_blank" href='https://XLNT.co'>XLNT</a> project
+        by <a target="_blank" href='https://zeppelin.solutions'>Zeppelin</a>
       </p>
     </div>
   </footer>
