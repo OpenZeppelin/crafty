a5029ad04b37a0de9e603e98b9319a78d0a47b0b
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-05-23 14:00:48 -0300

    Removed block number and time.

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -4,19 +4,15 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='grid-x grid-padding-x space-between flex-center'>
-  	<div className='flex-center'>
-  		<a target='_blank' href='https://github.com/zeppelinos/crafty' rel='noopener noreferrer'>
-  			<img alt='Github' className='github' src='./images/github.svg'/>
-  		</a>
-  		<div>
-	  		<span className='light'>Last block:</span>
-	  		<span className='strong'> #3212515 (mined 21 seconds ago)</span>
-  		</div>
-  	</div>
     <div className='cell auto'>
       <p className='light'>
         Powered by <a target='_blank' href='https://XLNT.co' rel='noopener noreferrer'>XLNT</a> and <a target='_blank' href='http://zeppelinos.org/' rel='noopener noreferrer'>ZeppelinOS</a>
       </p>
+    </div>
+    <div className='flex-center'>
+      <a target='_blank' href='https://github.com/zeppelinos/crafty' rel='noopener noreferrer'>
+        <img alt='Github' className='github' src='./images/github.svg'/>
+      </a>
     </div>
   </footer>
 )
