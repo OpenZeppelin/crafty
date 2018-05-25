19ec8e4998579c3d9e54ae96168a2cb5226f700d
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-05-25 11:52:32 -0300

    Added EmojiOne attribution.

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -4,6 +4,11 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='grid-x grid-padding-x space-between flex-center'>
+    <div className='flex-left'>
+      <p className='light'>
+        Emoji artwork provided by <a target='_blank' href='https://emojione.com' rel='noopener noreferrer'>EmojiOne</a>
+      </p>
+    </div>
     <div className='cell auto'>
       <p className='light'>
         Powered by <a target='_blank' href='https://XLNT.co' rel='noopener noreferrer'>XLNT</a> and <a target='_blank' href='http://zeppelinos.org/' rel='noopener noreferrer'>ZeppelinOS</a>
